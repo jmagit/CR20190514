b217a0011de90a67bbd3f5a43019d6cfa7a50214
Javier <jmartin@grupoloyal.com>
2019-05-14 15:06:32 +0200

    Hola Mundo

--- demos/src/main/java/com/example/demos/DemosApplication.java
+++ demos/src/main/java/com/example/demos/DemosApplication.java
@@ -1,13 +1,20 @@
 package com.example.demos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemosApplication {
+public class DemosApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo");
+		
 	}
 
 }
